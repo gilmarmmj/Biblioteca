eeb9dc4476845f0a1eed695af9aa5f8c2e0879f9
guest01 <guest01@10.1.49.158>
2013-10-02 11:28:35 -0300

    Atualizacao da classe Emprestimos.

--- src/br/com/gilmarmmj/model/Emprestimos.java
+++ src/br/com/gilmarmmj/model/Emprestimos.java
@@ -1,6 +1,79 @@
 
 package br.com.gilmarmmj.model;
 
+import java.util.Date;
+
 public class Emprestimos {
     
+    private int codigo;
+    private Cliente cliente;
+    private Livros livros;
+    private Date dataEmprestimo;
+    private Date dataDevolucao;
+    private double multa;
+    private String status;
+
+    public Emprestimos() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Livros getLivros() {
+        return livros;
+    }
+
+    public void setLivros(Livros livros) {
+        this.livros = livros;
+    }
+
+    public Date getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(Date dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public Date getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(Date dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public double getMulta() {
+        return multa;
+    }
+
+    public void setMulta(double multa) {
+        this.multa = multa;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Emprestimos{" + "codigo=" + codigo + ", cliente=" + cliente + ", livros=" + livros + ", dataEmprestimo=" + dataEmprestimo + ", dataDevolucao=" + dataDevolucao + ", multa=" + multa + ", status=" + status + '}';
+    }
 }
