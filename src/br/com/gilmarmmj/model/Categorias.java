d2c3b5b0e401a87db1d1e1c37977ddbf08551403
guest01 <guest01@10.1.49.158>
2013-10-02 10:37:19 -0300

    Atualizando a classe Categorias.

--- src/br/com/gilmarmmj/model/Categorias.java
+++ src/br/com/gilmarmmj/model/Categorias.java
@@ -3,4 +3,39 @@ package br.com.gilmarmmj.model;
 
 public class Categorias {
     
+    private int codigo;
+    private String nomeCategoria;
+    private String observacao;
+
+    public Categorias() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNomeCategoria() {
+        return nomeCategoria;
+    }
+
+    public void setNomeCategoria(String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Categorias{" + "codigo=" + codigo + ", nomeCategoria=" + nomeCategoria + ", observacao=" + observacao + '}';
+    }
 }
