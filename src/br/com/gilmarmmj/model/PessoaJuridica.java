f8c5f8c89614e0a854c621971fe70bfa57d8bcb7
guest01 <guest01@10.1.49.158>
2013-10-02 10:51:14 -0300

    Atualizacao da classe PessoaJuridica.

--- src/br/com/gilmarmmj/model/PessoaJuridica.java
+++ src/br/com/gilmarmmj/model/PessoaJuridica.java
@@ -1,13 +1,115 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.com.gilmarmmj.model;
 
-/**
- *
- * @author guest01
- */
-public class PessoaJuridica {
-    
+import java.util.Date;
+
+public abstract class PessoaJuridica {
+  
+    private int codigo;
+    private String nome;
+    private String cnpj;
+    private String ie;
+    private Endereco endereco;
+    private String telefone;
+    private String celular;
+    private String email;
+    private Date dataNascimento;
+    private Date dataCadastro;
+    private String status;
+
+    public PessoaJuridica() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getIe() {
+        return ie;
+    }
+
+    public void setIe(String ie) {
+        this.ie = ie;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "PessoaJuridica{" + "codigo=" + codigo + ", nome=" + nome + ", cnpj=" + cnpj + ", ie=" + ie + ", endereco=" + endereco + ", telefone=" + telefone + ", celular=" + celular + ", email=" + email + ", dataNascimento=" + dataNascimento + ", dataCadastro=" + dataCadastro + ", status=" + status + '}';
+    }
 }
