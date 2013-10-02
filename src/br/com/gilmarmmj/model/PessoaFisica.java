bfc0766cabbd607ec34bd41752c498029f137ecc
guest01 <guest01@10.1.49.158>
2013-10-02 10:50:39 -0300

    Atualizacao da classe PessoaFisica.

--- src/br/com/gilmarmmj/model/PessoaFisica.java
+++ src/br/com/gilmarmmj/model/PessoaFisica.java
@@ -1,13 +1,115 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.com.gilmarmmj.model;
 
-/**
- *
- * @author guest01
- */
+import java.util.Date;
+
 public class PessoaFisica {
-    
+   
+    private int codigo;
+    private String nome;
+    private String cpf;
+    private String rg;
+    private Endereco endereco;
+    private String telefone;
+    private String celular;
+    private String email;
+    private Date dataNascimento;
+    private Date dataCadastro;
+    private String status;
+
+    public PessoaFisica() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "PessoaFisica{" + "codigo=" + codigo + ", nome=" + nome + ", cpf=" + cpf + ", rg=" + rg + ", endereco=" + endereco + ", telefone=" + telefone + ", celular=" + celular + ", email=" + email + ", dataNascimento=" + dataNascimento + ", dataCadastro=" + dataCadastro + ", status=" + status + '}';
+    }
 }
