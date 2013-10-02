da8f6c6a17a1656397cc2127730c8a4b98774ff9
guest01 <guest01@10.1.49.158>
2013-10-02 11:05:49 -0300

    Atualizacao da classe Endereco.

--- src/br/com/gilmarmmj/model/Endereco.java
+++ src/br/com/gilmarmmj/model/Endereco.java
@@ -1,13 +1,94 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.gilmarmmj.model;
 
-/**
- *
- * @author guest01
- */
 public class Endereco {
-    
+
+    private int codigo;
+    private String pais;
+    private String estado;
+    private String cidade;
+    private String bairro;
+    private String rua;
+    private int numero;
+    private String cep;
+    private String observacao;
+
+    public Endereco() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco{" + "codigo=" + codigo + ", pais=" + pais + ", estado=" + estado + ", cidade=" + cidade + ", bairro=" + bairro + ", rua=" + rua + ", numero=" + numero + ", cep=" + cep + ", observacao=" + observacao + '}';
+    }
 }
