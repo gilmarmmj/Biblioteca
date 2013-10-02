749d7d723101257b73f15977343a097d18bb9b26
guest01 <guest01@10.1.49.158>
2013-10-02 11:06:19 -0300

    Atualizacao da classe Livros.

--- src/br/com/gilmarmmj/model/Livros.java
+++ src/br/com/gilmarmmj/model/Livros.java
@@ -1,7 +1,87 @@
 package br.com.gilmarmmj.model;
 
+import java.util.Date;
+
 public class Livros {
     
-    
-    
+   private int codigo;
+   private String titulo;
+   private Autores autores;
+   private Categorias categorias;
+   private Date dataPublicacao;
+   private String sinopse;
+   private String observacao;
+   private String status;
+
+    public Livros() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autores getAutores() {
+        return autores;
+    }
+
+    public void setAutores(Autores autores) {
+        this.autores = autores;
+    }
+
+    public Categorias getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(Categorias categorias) {
+        this.categorias = categorias;
+    }
+
+    public Date getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    public void setDataPublicacao(Date dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Livros{" + "codigo=" + codigo + ", titulo=" + titulo + ", autores=" + autores + ", categorias=" + categorias + ", dataPublicacao=" + dataPublicacao + ", sinopse=" + sinopse + ", observacao=" + observacao + ", status=" + status + '}';
+    }
 }
