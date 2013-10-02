19b4cfaaf04ee1800b9c04b09593a0cffa08caed
guest01 <guest01@10.1.49.158>
2013-10-02 10:37:04 -0300

    Atualizando a classe Autores.

--- src/br/com/gilmarmmj/model/Autores.java
+++ src/br/com/gilmarmmj/model/Autores.java
@@ -1,13 +1,40 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.gilmarmmj.model;
 
-/**
- *
- * @author guest01
- */
 public class Autores {
-    
+
+    private int codigo;
+    private int nomeAutor;
+    private String observacao;
+
+    public Autores() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getNomeAutor() {
+        return nomeAutor;
+    }
+
+    public void setNomeAutor(int nomeAutor) {
+        this.nomeAutor = nomeAutor;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Autores{" + "codigo=" + codigo + ", nomeAutor=" + nomeAutor + ", observacao=" + observacao + '}';
+    }
 }
