26af21977997685da357006c83b1d8b9e515bb01
guest01 <guest01@10.1.49.158>
2013-10-02 10:39:44 -0300

    Atualizacao da classe Emprestimos e Livros, e remocao da classe Pessoas.

--- src/br/com/gilmarmmj/model/Emprestimos.java
+++ src/br/com/gilmarmmj/model/Emprestimos.java
@@ -1,13 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.com.gilmarmmj.model;
 
-/**
- *
- * @author guest01
- */
 public class Emprestimos {
     
 }
--- src/br/com/gilmarmmj/model/Pessoa.java
+++
@@ -1,7 +0,0 @@
-package br.com.gilmarmmj.model;
-
-public abstract class Pessoa {
-    
-    private int codigo;
-    private String 
-}
